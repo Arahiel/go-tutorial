7c81ecd01c1a49449b237660def5f1d1d66c5bd4
Sebastian Tobolski <seba.tob124@gmail.com>
2021-04-01 16:58:19 +0200

    Implemented another way of handling gorountines - separate function, with recursive calling and buffered channel

--- playground/main.go
+++ playground/main.go
@@ -2,20 +2,40 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"os"
+	"net/http"
+	"time"
 )
 
 func main() {
-	args := os.Args
-
-	for _, filename := range args[1:] {
-		file, err := os.Open(filename)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-		io.Copy(os.Stdout, file)
-		fmt.Println()
+	linkList := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://amazon.com",
 	}
+
+	c := make(chan string, 1)
+
+	for _, link := range linkList {
+		go handleLink(link, c)
+	}
+
+	for {
+		time.Sleep(time.Millisecond * 100)
+	}
+}
+
+func handleLink(link string, c chan string) {
+	checkLink(link, c)
+	time.Sleep(time.Second * 5)
+	go handleLink(<-c, c)
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, " might be down!")
+		c <- link
+	}
+	fmt.Println(link, " is OK!")
+	c <- link
 }
